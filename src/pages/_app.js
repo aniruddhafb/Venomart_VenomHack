a48d71ee5eef7a31708fcd70aedbbc79acd74406
Aniruddha <aniruddhavikharankarfb@gmail.com>
2023-05-25 18:11:17 +0530

    footer added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
+import "@/styles/bootstrap.css";
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
