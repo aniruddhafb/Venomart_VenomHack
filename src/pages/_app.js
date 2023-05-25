b77d1e1a7fd9912684f403a1a8d36b1920ce595f
Aniruddha <aniruddhavikharankarfb@gmail.com>
2023-05-25 16:47:36 +0530

    initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar
+        theme={"dark"}
+      />
+      <Component {...pageProps}
+        theme={"dark"} />
+      <Footer
+        theme={"dark"}
+      />
+    </>
+  )
 }
